b9a49057e4b7ddbf22ea01f7cb0782b57eb942a7
Robbie Leslie <rwl228@cornell.edu>
2026-02-24 01:03:06 -0500

    Finished Lab 3 report

--- Lab3/include/distance_functions.h
+++ Lab3/include/distance_functions.h
@@ -6,6 +6,11 @@
 
 #define SETUP_TRIES 5
 
+struct Distances{
+  int front;
+  int side;
+};
+
 inline int getSensorDistance(SFEVL53L1X &sensor){
 
   if(sensor.checkForDataReady()){
@@ -16,6 +21,57 @@ inline int getSensorDistance(SFEVL53L1X &sensor){
     return -1; //-1 is data not ready error
   }
   
+}
+
+inline bool bothDistancesIfReady(SFEVL53L1X &front, SFEVL53L1X &side)
+{
+  bool fr = front.checkForDataReady();
+  bool sr = side.checkForDataReady();
+  if (!(fr && sr)) return false;
+
+  return true;
+}
+
+inline bool readBothDistancesIfReady(SFEVL53L1X &front, SFEVL53L1X &side, Distances &out)
+{
+  bool fr = front.checkForDataReady();
+  bool sr = side.checkForDataReady();
+  if (!(fr && sr)) return false;
+
+  out.front = front.getDistance();
+  out.side  = side.getDistance();
+
+  front.clearInterrupt();
+  side.clearInterrupt();
+  return true;
+}
+
+inline Distances getDistances(SFEVL53L1X &frontSensor, SFEVL53L1X &sideSensor){
+  Distances dists = {-1, -1};
+  
+  int front_dist = getSensorDistance(frontSensor);
+  int side_dist = getSensorDistance(sideSensor);
+
+  if(front_dist != -1){
+    dists.front = front_dist;
+  }
+
+  if(side_dist != -1){
+    dists.side = side_dist;
+  }
+
+  return dists;
+}
+
+inline void updateDistance(Distances &out, SFEVL53L1X &frontSensor, SFEVL53L1X &sideSensor){
+
+  if(frontSensor.checkForDataReady()){
+    out.front = getSensorDistance(frontSensor);
+  }
+
+  if(sideSensor.checkForDataReady()){
+    out.side = getSensorDistance(sideSensor);
+  }
 }
 
 bool setupSensor(SFEVL53L1X &sensor, bool alternate);
--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -6,6 +6,11 @@
 #include "SparkFun_VL53L1X.h"
 #include "distance_functions.h"
 
+#ifdef IMU
+#include <ICM_20948.h>
+#include "imu_functions.h"
+#endif
+
 //Optional interrupt and shutdown pins.
 #define SHUTDOWN_PIN 2
 #define INTERRUPT_PIN 3
@@ -15,7 +20,19 @@ SFEVL53L1X distanceSensorSide;
 //Uncomment the following line to use the optional shutdown and interrupt pins.
 //SFEVL53L1X distanceSensorFront(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
 
+Distances dists = {-1, -1};
+
+#ifdef IMU
+ICM_20948_I2C myICM; // Create an ICM_20948_I2C object
+#endif
+
 int prev_time = 0;
+
+uint32_t last_report_us = 0;
+uint32_t loop_count = 0;
+
+uint16_t front_mm = 0, side_mm = 0;
+uint32_t front_updates = 0, side_updates = 0;
 
 inline void printFrontSensor(){
   distanceSensorFront.startRanging(); //Write configuration bytes to initiate measurement
@@ -59,6 +76,10 @@ void setup(void)
       while(1); // TODO maybe change this and set some kind of bluetooth status checker
   }
 
+  #ifdef IMU
+  initIMU(myICM);
+  #endif
+
   // pinMode(A0, OUTPUT); //Used to control xshut on the side ToF
   // digitalWrite(A0, LOW);  // Xshut low to set I2C address
 
@@ -94,15 +115,71 @@ void loop(void)
   // int front_dist = getSensorDistance(distanceSensorFront);
   // int side_dist = getSensorDistance(distanceSensorSide);
 
-  if(distanceSensorFront.checkForDataReady() && distanceSensorSide.checkForDataReady()){
-    Serial.println(micros() - prev_time); // printing clock
-    prev_time = micros();
-  }
+  // if(distanceSensorFront.checkForDataReady() && distanceSensorSide.checkForDataReady()){
+  //   distanceSensorFront.clearInterrupt();
+  //   distanceSensorSide.clearInterrupt();
+  //   uint32_t now = micros();
+  //   Serial.println(now - prev_time); // printing clock
+  //   prev_time = now;
+  // }
 
-  //Serial.println(front_dist);
-  //Serial.println(side_dist);
+  // loop_count++;
+
+  // // --- Poll sensors, non-blocking ---
+  // if (distanceSensorFront.checkForDataReady()) {
+  //   front_mm = distanceSensorFront.getDistance();   // consume new sample
+  //   distanceSensorFront.clearInterrupt();           // ack/clear ready
+  //   // If your library requires it: distanceSensorFront.startRanging();
+  //   front_updates++;
+  // }
+
+  // if (distanceSensorSide.checkForDataReady()) {
+  //   side_mm = distanceSensorSide.getDistance();
+  //   distanceSensorSide.clearInterrupt();
+  //   // If your library requires it: distanceSensorSide.startRanging();
+  //   side_updates++;
+  // }
+
+  // // --- Periodic reporting (DO NOT print every iteration) ---
+  // uint32_t now = micros();
+  // if (now - last_report_us >= 100000) { // 100 ms
+  //   float seconds = (now - last_report_us) / 1e6f;
+  //   float loop_hz = loop_count / seconds;
+
+  //   Serial.print("loop_hz=");
+  //   Serial.print(loop_hz, 1);
+  //   Serial.print(" front_hz=");
+  //   Serial.print(front_updates / seconds, 1);
+  //   Serial.print(" side_hz=");
+  //   Serial.println(side_updates / seconds, 1);
+
+  //   last_report_us = now;
+  //   loop_count = 0;
+  //   front_updates = 0;
+  //   side_updates = 0;
+  // }
+
+  
   //Serial.println(""); //just for formatting, does make it slower
 
   //printFrontSensor();
+
+  updateDistance(dists, distanceSensorFront, distanceSensorSide);
+
+  Serial.print("Front: ");
+  Serial.print(dists.front);
+  Serial.print("    Side: ");
+  Serial.print(dists.side);
+  Serial.println("");
+
+  #ifdef IMU
+  Serial.print(dists.front);
+  Serial.print(",");
+  Serial.print(dists.side);
+  Serial.print(",");
+
+  myICM.getAGMT();
+  printSerialPlot(&myICM);
+  #endif
 }
 
